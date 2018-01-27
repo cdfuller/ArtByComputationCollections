f6147af76fd944e80191d99f2484115cec69eb57
Cody Fuller <git@codyfuller.com>
2018-01-26 22:19:54 -0600

    12 - Init

---
+++ Perlin-Flow-Fields/12/particle.js
@@ -0,0 +1,103 @@
+MIN_LIFESPAN = 300;
+MAX_LIFESPAN = 2500;
+
+function Particle(x, y, c) {
+  this.pos = createVector(x, y);
+  this.vel = createVector(random(-TWO_PI, TWO_PI), random(-TWO_PI, TWO_PI));
+  this.acc = createVector(0, 0);
+  this.maxSpeed = 1;
+  this.diameter = 2;
+  this.lifetime = random(MIN_LIFESPAN, MAX_LIFESPAN);
+  this.age = 0;
+  this.strokeColor = 0;
+  this.strokeAlpha = 255;
+  this.strokeWeight = 0.5 ;
+  this.color = c;
+
+  this.prevPos = this.pos.copy();
+
+  this.update = function updateParticle() {
+    if (this.age < this.lifetime) {
+      this.vel.add(this.acc);
+      this.vel.limit(this.maxSpeed);
+      this.prevPos = this.pos.copy();
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+      this.edges();
+      this.age += 1;
+    } else {
+      let { startX, startY } = getStartPosition();
+      this.pos = createVector(startX, startY);
+      this.prevPos = this.pos.copy();
+      this.vel = createVector(0, 0);
+      this.acc = createVector(0, 0);
+      this.lifetime = random(MIN_LIFESPAN, MAX_LIFESPAN)
+      this.age = 0;
+      this.color = getColor()
+      total_particles++;
+    }
+  }
+
+  this.applyForce = function applyForceParticle(force) {
+    this.acc.add(force);
+  }
+
+  this.follow = function followParticle(flowfield) {
+    let x = floor(this.pos.x / flowfield.fieldSize);
+    let y = floor(this.pos.y / flowfield.fieldSize);
+    let idx = x + y * flowfield.cols;
+    let force = flowfield.field[idx];
+    this.applyForce(force);
+  }
+
+  this.show = function showParticle() {
+    // if (frameCount > 0 && frameCount % 500 == 0) {
+    //   this.strokeColor = min(abs(this.strokeColor + 100), 255);
+    //   this.strokeWeight = this.strokeWeight - 0.75;
+    // }
+    if (this.age > 5 && frameCount > 0) {
+      // line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+      let direction = p5.Vector.sub(this.prevPos, this.pos);
+      stroke(this.color);
+      strokeWeight(this.strokeWeight);
+      drawShape(this.pos.x, this.pos.y, direction.heading())
+      // strokeJoin(ROUND);
+      // noFill();
+      // noStroke();
+      fill(this.color);
+      // ellipse(this.pos.x, this.pos.y, this.diameter);
+    }
+  }
+
+  this.edges = function() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.prevPos = this.pos.copy();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.prevPos = this.pos.copy();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.prevPos = this.pos.copy();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.prevPos = this.pos.copy();
+    }
+  }
+}
+
+function drawShape(x, y, heading) {
+  push();
+  translate(x, y);
+  rotate(heading);
+  beginShape();
+  vertex(0, 20);
+  vertex(20, 20);
+  vertex(20, 0);
+  vertex(0, 0);
+  endShape(CLOSE);
+  pop();
+}
---
+++ Perlin-Flow-Fields/12/sketch.js
@@ -0,0 +1,114 @@
+let FIELD_SIZE = 50;
+
+let particles = [];
+let PARTICLE_COUNT = 500;
+let total_particles = PARTICLE_COUNT;
+
+let flowfield;
+let starting_points;
+
+// let maxDist;
+
+
+function setup() {
+  createCanvas(800, 800);
+  // colorMode(HSB)
+  background(255);
+  noiseSeed(1002);
+  noiseDetail(16); 
+  starting_points = [
+    {x: width/4, y: height/4},
+    {x: width/4*3, y: height/4},
+    {x: width/4*3, y: height/4*3},
+    {x: width/4, y: height/4*3},
+  ]
+
+  flowfield = new FlowField(FIELD_SIZE);
+  // maxDist = ((width / 2) ** 2 + (height / 2) ** 2) ** 0.5;
+
+  for (let i = 0; i < PARTICLE_COUNT; i++) {
+    let {startX, startY} = getStartPosition();
+    let c = getColor();
+    particles[i] = new Particle(startX, startY, c);
+  }
+}
+
+function draw() {
+  // flowfield.update();
+
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].show();
+  }
+  
+  if (frameCount % 250 == 0) {
+    printStatus();  
+  }
+}
+
+function getStartPosition() {
+  let f = 8;
+  // let startX = random(width / f, width / f * (f - 1));
+  // let startY = random(height / f, height / f * (f - 1));
+  let startX = random(width);
+  let startY = random(height);
+  // startX = startX + (width/f) * 0.5;
+  // startY = startY + (height/f) * 0.5;
+  return {startX, startY};
+}
+
+
+function getColor() {
+  let a = 255;
+  let palette = [
+    [76, 175, 80, a],
+    [124, 77, 255, a],
+    [3, 169, 244, a],
+    [255, 235, 59, a],
+  ]
+  let c = random(palette);
+  // c[3] = c[3] + 4;
+  return c;
+}
+
+function printStatus() {
+  let fc = frameCount
+  let avg = averageAge().toFixed(2);
+  let ls = averageLifespan().toFixed(2);
+  let tot = total_particles;
+  let fr = frameRate().toFixed(2);
+  let s = `${fc}\tavg: ${avg} ls: ${ls} total: ${tot} fr: ${fr}`
+  console.log(s);
+}
+
+function averageAge() {
+  return particles.reduce((prev, curr) => prev + int(curr.age), 0) / particles.length;
+}
+
+function averageLifespan() {
+  return particles.reduce((prev, curr) => prev + int(curr.lifetime), 0) / particles.length;
+}
+
+function keyPressed() {
+  switch (key) {
+    case 'S':
+      loop();
+      console.log("Started looping");
+      break;
+    case ' ':
+      noLoop();
+      console.log("Stopped looping");
+      break;
+    default:
+      break;
+  }
+}
+
+function mousePressed() {
+  // let { startX, startY } = getStartPosition();
+  let c = getColor();
+  let p = new Particle(mouseX, mouseY, c);
+  particles.push(p);
+  total_particles++;
+}
