cbdf8b2ce909880a22a598cf194e62005de55af7
Cody Fuller <git@codyfuller.com>
2018-02-16 22:40:40 -0600

    33 - Init

---
+++ Perlin-Flow-Fields/33/field.js
@@ -0,0 +1,43 @@
+function FlowField(fieldSize) {
+  let xIncr = 0.1;
+  let yIncr = xIncr;
+  let zIncr = 0.02;
+  // let zOff = 10.02;
+  let zOff = 0.0;
+
+  this.fieldSize = fieldSize;
+  this.cols = floor(width / this.fieldSize);
+  this.rows = floor(height / this.fieldSize);
+
+  this.field = new Array(this.cols * this.rows);
+
+  this.update = function updateFlowField() {
+    let yOff = 0.0;
+    for (let y = 0; y < this.rows; y++) {
+      let xOff = 0;
+      for (let x = 0; x < this.cols; x++) {
+        let idx = x + (y * this.cols);
+        let angle = generateFieldAngle(xOff, yOff, zOff);
+        let v = p5.Vector.fromAngle(angle);
+        // v.setMag(0.5);
+        this.field[idx] = v;
+
+        xOff += xIncr;
+      }
+      yOff += yIncr;
+    }
+
+    if (frameCount % 10 == 0) {
+      zOff += zIncr;
+    }
+  }
+
+  this.update();
+
+  function generateFieldAngle(xOff, yOff, zOff) {
+    let n = noise(xOff, yOff, zOff); // 0 < n < 1
+    // let f = 9;
+    // n = floor(n * f) / f;
+    return - n * TWO_PI + 0.39;
+  }
+}
