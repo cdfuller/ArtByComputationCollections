c043243569c0c66f9140bb92cd79c43b06c0efd2
Cody Fuller <git@codyfuller.com>
2018-02-24 01:28:21 -0600

    update to proper seed

--- Mandala/04/sketch.js
+++ Mandala/04/sketch.js
@@ -1,7 +1,7 @@
 // Factors of  1200:
 // 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 16, 20, 24, 25, 30, 40, 48, 50, 60, 75, 80, 100, 120, 150, 200, 240, 300, 400, 600, 1200
 //
-const NOISE_SEED = 10003;
+const NOISE_SEED = 10006;
 const RANDOM_SEED = NOISE_SEED;
 const NUM_FRAMES = 5000;
 const DEBUG_MODE = true;
