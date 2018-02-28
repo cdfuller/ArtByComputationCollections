3bf04ef59c62ec2585bf0abd76f72e18f6cb630c
Cody Fuller <git@codyfuller.com>
2018-02-27 18:52:56 -0600

    Mandala - 08 - Init

---
+++ Mandala/08/sketch.js
@@ -0,0 +1,78 @@
+// Factors of  1200:
+// 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 16, 20, 24, 25, 30, 40, 48, 50, 60, 75, 80, 100, 120, 150, 200, 240, 300, 400, 600, 1200
+//
+const NOISE_SEED = 10010;
+const RANDOM_SEED = NOISE_SEED;
+const NUM_FRAMES = 5000;
+const DEBUG_MODE = true;
+
+let RADIUS;
+let LOBE_COUNT = 25;
+let mandala;
+
+function setup() {
+  createCanvas(1200, 1200);
+  background(31, 42, 62);
+  // background(255);
+  noiseSeed(NOISE_SEED);
+  randomSeed(RANDOM_SEED);
+
+  rectMode(CENTER);
+
+  RADIUS = min(width, height)/2;
+  mandala = new Mandala(LOBE_COUNT);
+}
+
+function draw() {
+
+  mandala.update();
+  mandala.draw();
+  noLoop();
+
+  if (DEBUG_MODE) {
+    if (frameCount % 100 == 0) printStatus();
+    if (frameCount == NUM_FRAMES) noLoop();
+  }
+}
+
+function printStatus() {
+}
+
+function keyPressed() {
+  switch (key) {
+    case 'S':
+      loop();
+      console.log("Started looping");
+      break;
+    case ' ':
+      noLoop();
+      console.log("Stopped looping");
+      printStatus();
+      break;
+    default:
+      break;
+  }
+}
+
+
+let colorIndex = 0;
+function getColor() {
+  let gentle_pride = [
+    [242, 82, 53],
+    [238, 170, 86],
+    [238, 232, 96],
+    [100, 242, 138],
+    [99, 148, 231],
+  ]
+let vibrant = [
+  [23, 222, 238],
+  [255, 127, 80],
+  [255, 65, 98],
+  [236, 242, 132],
+  [16, 174, 178],
+]
+  let palette = vibrant;
+  let c = palette[colorIndex++ % palette.length];
+  // let c = [0, 0, 0];
+  return [...c, 255];
+}
