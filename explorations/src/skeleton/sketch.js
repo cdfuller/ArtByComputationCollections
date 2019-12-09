dd31c6e1946e1c57e225ed0742ee5a64bcb0bee2
Cody Fuller <git@codyfuller.com>
2019-12-08 23:22:17 -0600

    Add explorations skeleton

---
+++ explorations/src/skeleton/sketch.js
@@ -0,0 +1,10 @@
+/// <reference path="../../node_modules/@types/p5/global.d.ts" />
+
+function setup() {
+    createCanvas(600, 600);
+    background(51);
+}
+
+function draw() {
+    ellipse(width/2, height/2, 10);
+}
